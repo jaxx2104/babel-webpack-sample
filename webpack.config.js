400342ee4c50576b062c8dd72ed8ad993ac3f6d9
iwa <jaxx2104@gmail.com>
2017-05-09 00:48:19 +0900

    Fix ignore

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,23 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
-    entry: {
-        app: './src/app.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            },
-        ],
-    }
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+    ],
+  }
 }
